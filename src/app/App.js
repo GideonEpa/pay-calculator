c9a35004e6b029a4a163cb70b4cd2decaae5cc9b
GideonEpa <gideon.epa@outlook.com>
2024-08-21 15:36:50 +1000

    Cleanup file structure and remove defaults

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Calculator from "../calculator/Calculator.jsx"
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Calculator />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/calculator/Calculator.jsx
@@ -0,0 +1,5 @@
+export default function Calculator() {
+    return (
+        <h2>Hi</h2>
+    )
+}
